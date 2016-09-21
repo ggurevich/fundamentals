765d5c01062ca86d36d46b16c89339508208f1ae
ggurevich <gurevichgabriel@gmail.com>
2016-09-21 10:30:13 -0400

    added store project and javascript

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,45 @@
+// var num = 7;
+// var newNum = 5; //this is camel casing
+// var newer_num = 10; //this is snake sasing
+
+// var x=5;
+// console.log(num);
+
+// var a=5;
+// var b=10;
+// console.log(a*b);
+
+// var c=a+b;
+// console.log(c);
+
+
+// console.log(200/40);
+// console.log(10 % 5);
+// console.log(4 % 3);
+// console.log(2+2);
+
+var newNum =10;
+newNum=newNum +1;
+console.log(newNum);
+newNum++;
+console.log(newNum);
+
+newNum=newNum-1;
+console.log(newNum);
+newNum--;
+console.log(newNum);
+
+
+newNum=newNum+10;
+console.log(newNum);
+
+newNum+=10;
+console.log(newNum);
+
+newNum-=20;
+console.log(newNum);
+
+newNum*=10
+console.log(newNum);
+newNum/=10
+console.log(newNum)

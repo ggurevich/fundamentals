df6cd9e179d26505d2c70e19b72122cc3e5acdfa
ggurevich <gurevichgabriel@gmail.com>
2016-09-21 11:19:44 -0400

    add string file

---
+++ jsbasics/0.02_strings.js
@@ -0,0 +1,25 @@
+var birthCity ="i was born in indianapolis.";
+var birthState= "indiana";
+var comma = ",";
+console.log(birthCity,birthState);
+
+var yearGraduation = "2007";
+var school = "ged";
+console.log("I graduated from" + " " +school+ comma + yearGraduation);
+
+var username = "jamespauloconnor";
+var password = "123LetMeIn!";
+var tweet = "kanye is broke again";
+var facebookPost = "I love coding!";
+var instaCaption = "#Awesome";
+
+console.log(username);
+
+console.log("hello world");
+
+var make= "toyota";
+var model = "prius";
+var year = "2012";
+var space = " ";
+var car =make + space + model + space + year;
+console.log(car);

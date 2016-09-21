32d939c1fe65e03f7ca6c3a38e6b1f5f524a88fd
ggurevich <gurevichgabriel@gmail.com>
2016-09-21 15:38:23 -0400

    functions added

---
+++ jsbasics/0.06_functions.js
@@ -0,0 +1,51 @@
+// delclaration
+function helloWorld(){
+	console.log("hello world");
+}
+
+helloWorld();
+
+function printMyName(){
+	console.log("gabriel");
+}
+
+printMyName();
+
+function addNumbers(){
+	var x=10;
+	var y=10;
+	console.log(x+y);
+}
+
+function multipyNumbers(){
+	var x=10;
+	var y=10;
+	console.log(x*y);
+}
+
+//parameters and arguments
+
+function printNumber(x){
+	console.log("the number you entered was: " + x);
+}
+
+printNumber(6)
+
+function bankPin(x){
+	console.log(x+ " is the correct number.");
+}
+
+bankPin(12345)
+
+//bankpin
+	function enterpin(xyz) {
+		var pin = 503;
+
+		if (pin != xyz) {
+			console.log("Pin not accepted");
+		} else {
+			console.log("Correct!");
+		}
+	}
+
+	enterpin(502);

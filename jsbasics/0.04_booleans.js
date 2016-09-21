678638d3452a3fcba62505e01872864666f5484b
ggurevich <gurevichgabriel@gmail.com>
2016-09-21 11:55:03 -0400

    boolean

---
+++ jsbasics/0.04_booleans.js
@@ -0,0 +1,26 @@
+//booleans are true/false values and help with flow control and logic
+
+//var hangry = true;
+//console.log(hangry);
+
+//operators
+//== equals
+// === strick equality
+//!= inequality
+//!== strict inequality
+//>greater than
+//>= greater or equal
+//<less than
+//<=less or equal
+
+// var num = 5<2;
+// console.log(num);
+
+// var newNumb=5>2;
+// console.log(newNumb);
+
+// var numberOne= 1;
+// var numberTwo="2";
+
+// console.log((numberOne===numberTwo));
+

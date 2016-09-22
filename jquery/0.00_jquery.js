6c0aaf91f43f1187fa2f3b329345bf41ff5921e6
ggurevich <gurevichgabriel@gmail.com>
2016-09-22 15:04:17 -0400

    jquery items

---
+++ jquery/0.00_jquery.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	//alert("hello world");
+
+	$("#hide").click(function(){
+		$("#image").hide();
+	});
+	$("#show").click(function(){
+		$("#image").show();
+	});
+	$("#fadeIn").click(function(){
+		$("#image").fadeIn(50000);
+	});
+	$("#explode").click(function(){
+		$("#image").hide("explode", { pieces: 512}, 5000);
+	});
+	$("#slideIN").click(function(){
+		$("#image").slideUp(2000).slideDown(2000);
+	});
+	$("#pulse").click(function(){
+		$("#image").pulse("pulsate");
+	});
+});
